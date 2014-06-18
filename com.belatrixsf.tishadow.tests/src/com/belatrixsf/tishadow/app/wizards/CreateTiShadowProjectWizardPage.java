1166ae6de9a3031f168a62481b8e68f1fcb99d92
vvillegas <urseza@hotmail.com>
2014-06-18 16:00:40 -0300

    Added new classes to refactor code.

---
+++ com.belatrixsf.tishadow.tests/src/com/belatrixsf/tishadow/app/wizards/CreateTiShadowProjectWizardPage.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.belatrixsf.tishadow.app.wizards;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * @author vvillegas
+ * 
+ */
+public class CreateTiShadowProjectWizardPage extends TiShadowWizardPage{
+
+	protected CreateTiShadowProjectWizardPage(String pageName, String title,
+			ImageDescriptor titleImage) {
+		super(pageName, title, titleImage);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void addExtraFields() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
