3efe0abdd1fc8d6faaff4eaa86005c071d510b98
emilianoHualpa <ehualpa@gmail.com>
2014-08-28 10:18:46 -0300

    Updated tooltip, labels and help. Renamed all Run commands to Deploy.

--- com.belatrixsf.tishadow.help/src/javascript/ui.js
+++ com.belatrixsf.tishadow.help/src/javascript/ui.js
@@ -2,16 +2,23 @@ $(function(){
 	$("#items").stoc({
 		search: "#main_content_wrap"	
 	});
-});
 
-$( window ).scroll( function() {
-	
-	var $topLink = $("#top-link");
+	var $topLink  = $("#top-link");
 
-	if ($ (this).scrollTop() == 0){
-		$topLink.fadeOut();
-	} else {
-		$topLink.fadeIn();
-	}
-	
+	// hide top button on top
+	$( window ).scroll( function() {
+		if ($ (this).scrollTop() == 0){
+			$topLink.fadeOut();
+		} else {
+			$topLink.fadeIn();
+		}
+	});
+
+	// scroll body to 0px on click
+	$topLink.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 250);
+		return false;
+	});
 });
